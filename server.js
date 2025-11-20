5c3c1c3c92bc1f676c5319861d4ea607469390bc
Waris41506 <yusuffwaris8@gmail.com>
2025-11-20 13:06:46 +0100

    new1

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
+
 // -----------------------------
 // POST /send-code
 // -----------------------------
